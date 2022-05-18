45a4592d05d0a06c8d8aecb057e56b7d26cf0b5e
Loïc Brisset <loic@boreal-business.net>
2022-05-18 18:33:31 +0200

    invitee props

--- src/Models/Invitee.php
+++ src/Models/Invitee.php
@@ -3,12 +3,33 @@
 namespace LoBrs\Calendly\Models;
 
 use LoBrs\Calendly\Traits\ChildListable;
+use LoBrs\Calendly\Traits\Timeable;
 
+/**
+ * @property $uri
+ * @property $email
+ * @property $first_name
+ * @property $last_name
+ * @property $name
+ * @property $status
+ * @property $timezone
+ * @property $event
+ * @property $text_reminder_number
+ * @property $rescheduled
+ * @property $old_invitee
+ * @property $cancel_url
+ * @property $reschedule_url
+ * @property $cancellation
+ * @property $payment
+ * @property $no_show
+ * @property $reconfirmation
+ */
 class Invitee extends BaseModel
 {
     public static string $resource = "invitees";
 
     use ChildListable;
+    use Timeable;
 
     static function getParentResource() {
         return Event::$resource;
