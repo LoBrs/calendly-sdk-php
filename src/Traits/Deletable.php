c8a3f58729368e3cb99fc6ad39180dab76cdfdb2
Loïc Brisset <loic@boreal-business.net>
2022-08-09 15:41:39 +0200

    fix delete trait

--- src/Traits/Deletable.php
+++ src/Traits/Deletable.php
@@ -3,17 +3,19 @@
 namespace LoBrs\Calendly\Traits;
 
 use LoBrs\Calendly\Calendly;
+use LoBrs\Calendly\Exceptions\ApiErrorException;
+use LoBrs\Calendly\Exceptions\InvalidArgumentException;
+use LoBrs\Calendly\Exceptions\MissingCalendlyTokenException;
 
 trait Deletable
 {
-    public static function delete(array $options = []): bool {
-        try {
-            $response = Calendly::getClient()->request(static::$resource, "DELETE", $options);
-            return true;
-        } catch (\Exception $e) {
-            //
-        }
-
-        return false;
+    /**
+     * @throws InvalidArgumentException
+     * @throws ApiErrorException
+     * @throws MissingCalendlyTokenException
+     */
+    public function delete(array $options = []): bool {
+        Calendly::getClient()->request(static::$resource . "/" . $this->getId(), "DELETE", $options);
+        return true;
     }
 }
